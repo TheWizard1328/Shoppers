b7c97b2942d8aeb4e081b164fb87d1cf59b28699
Robert Tauber <tauberr1328@gmail.com>
2025-12-06 10:06:44 +0000

    Manual code change

--- src/components/common/StopCard.jsx
+++ src/components/common/StopCard.jsx
@@ -1138,7 +1138,7 @@ export default function StopCard({
                               </Select>
 
                               <div className="relative flex-1">
-                                <span className="absolute left-2 top-1/2 -translate-y-1/2 text-xs text-slate-500">$</span>
+                                <span className="absolute left-2 top-1/2 -translate-y-1/2 text-xs bg-white text-slate-500">$</span>
                                 <input
                             ref={(el) => codAmountInputRefs.current[index] = el}
                             type="text"
