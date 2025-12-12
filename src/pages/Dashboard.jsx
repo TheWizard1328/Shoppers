f1b3de94afefab1852f257cfbdc9b2c20e91cc09
Robert Tauber <tauberr1328@gmail.com>
2025-12-12 06:10:00 +0000

    Manual code change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1767,7 +1767,7 @@ function Dashboard() {
             bounds: allCoordinates,
             options: {
               paddingTopLeft: isMobile ? [125, StatsCardHeight] : [50, 50],
-              paddingBottomRight: [50, 245],
+              paddingBottomRight: [50, 100],
               maxZoom: phase1MaxZoom
             }
           });
