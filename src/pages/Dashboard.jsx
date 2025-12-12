5a5fa7b4b7259faca6a5763f1bfb9a5ebbf420c8
Robert Tauber <tauberr1328@gmail.com>
2025-12-12 06:33:11 +0000

    Manual code change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -60,7 +60,7 @@ import DashboardOfflineSync from '@/components/dashboard/DashboardOfflineSync';
 
   const STOP_CARDS_EXPANDED_HEIGHT = 450; // Expanded card height for map padding
   const STOP_CARDS_BASE_HEIGHT = 145; // Fixed non-expanded height for map padding
-  const StopCardsHeight = STOP_CARDS_BASE_HEIGHT;
+  const StopCardsHeight = STOP_CARDS_BASE_HEIGHT - 50;
 
   const STATS_CARD_BASE_HEIGHT = 116; // Fixed non-expanded height for map padding
   const STATS_CARD_EXTENDED_HEIGHT = 216; // Fixed non-expanded height for map padding
