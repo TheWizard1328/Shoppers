22703e0c3921be688ebe3b56539f1050ef4118ef
Robert Tauber <tauberr1328@gmail.com>
2025-12-16 16:24:34 +0000

    Manual code change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -343,7 +343,7 @@ function Dashboard() {
   const getMapPadding = useCallback((cardExpanded = false) => {
     const topPadding = isMobile 
       ? (isExpanded ? STATS_CARD_EXTENDED_HEIGHT - 20 : STATS_CARD_BASE_HEIGHT - 20) 
-      : -20;
+      : -50;
     const bottomPadding = isMobile 
       ? (cardExpanded ? STOP_CARDS_EXPANDED_HEIGHT + 20 : STOP_CARDS_BASE_HEIGHT + 50)
       : 50;
