dcd1bbd02c176cfe063eef48c4dd784361483056
Robert Tauber <tauberr1328@gmail.com>
2025-12-16 15:29:00 +0000

    Manual code change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -346,7 +346,7 @@ function Dashboard() {
       : 20;
     const bottomPadding = isMobile 
       ? (cardExpanded ? STOP_CARDS_EXPANDED_HEIGHT + 50 : STOP_CARDS_BASE_HEIGHT - 20)
-      : 40;
+      : 50;
     return {
       paddingTopLeft: [25, topPadding],
       paddingBottomRight: [25, bottomPadding]
