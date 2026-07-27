52d9a8be7d4fc3340683e591ee2604efe450e6d4
Robert Tauber <tauberr1328@gmail.com>
2026-07-27 07:03:44 +0000

    Auto-commit sub-agent changes

--- src/functions/repairMissingPolylines.js
+++
@@ -1,7 +0,0 @@
-import { base44 } from '@/api/base44Client';
-
-export async function repairMissingPolylines(payload) {
-  return await base44.functions.invoke('repairMissingPolylines', payload || {});
-}
-
-export default repairMissingPolylines;
